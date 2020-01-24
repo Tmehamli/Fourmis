7c7115d41bc184820bf14ba111f8b07d95938b12
tmehamli <tmehamli@ensc.fr>
2020-01-24 01:27:35 +0100

    a new world

--- World.py
+++ World.py
@@ -1,26 +1,37 @@
-import random as rd
 import numpy as np
-rd.seed(42)
+np.random.seed(42)
+class World:
+    def __init__(self, nb_cities, nb_ants):
+        self.nb_cities = nb_cities
+        self.nb_ants = nb_ants
+        self.map = None
+        self.c = 0.01
+        self.initialize()
 
-class Monde():
-    def __init__(self):
-        rd.seed(42)
-        GAMMA = rd.uniform(0,1)
-        self.Villes = ['A', 'B', 'C','D', 'E', 'F']
-        self.Plan_gam_distance_phero = {
-            (self.Villes[0],self.Villes[1]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[0],self.Villes[2]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[0],self.Villes[3]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[0],self.Villes[4]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[0],self.Villes[5]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[1],self.Villes[2]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[1],self.Villes[3]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[1],self.Villes[4]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[1],self.Villes[5]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[2],self.Villes[3]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[2],self.Villes[4]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[2],self.Villes[5]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[3],self.Villes[4]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[3],self.Villes[5]): np.asarray([GAMMA, rd.uniform(1, 20),.01]),
-            (self.Villes[4],self.Villes[5]): np.asarray([GAMMA, rd.uniform(1, 20),.01])
-        }
+    def initialize(self):
+        self.creat_map()
+        self.creat_phe_map()
+        self.creat_place_ants()
+
+    def creat_map(self):
+        # self.map = np.arange()
+        _map = np.random.randint(0, self.nb_cities*2, (self.nb_cities,self.nb_cities))
+        _map -= np.diag(_map.diagonal())
+        _map = _map + _map.T
+        self.map = _map
+
+    def creat_phe_map(self):
+        _phe_map = np.full((self.nb_cities,self.nb_cities), self.c)
+        _phe_map -= np.diag(_phe_map.diagonal())
+        print(_phe_map)
+
+    def run_one_cycle(self):
+        pass
+
+    def creat_place_ants(self):
+        pass
+    
+if __name__ == '__main__':
+    nb_cities = 5
+    nb_ants = 1
+    world = World(nb_cities, nb_ants)
