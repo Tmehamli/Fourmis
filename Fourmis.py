b8ad369d001e6b43f401f2aac375b214d827870a
tmehamli <tmehamli@ensc.fr>
2020-01-25 17:31:31 +0100

    make choice and creat place ants

--- Fourmis.py
+++ Fourmis.py
@@ -1,8 +1,35 @@
+import numpy as np
+import math as m
 class Fourmi:
-    def __init__(self):
+    def __init__(self, iden, stat_pos):
+        self.identity = iden
         self.memory = []
-        self.start_position = None
-        self.initialize()
+        self.start_position = stat_pos
+        self.t = 0
+        # self.initialize()
 
     def initialize(self):
         pass
+
+    def make_choice(self, i_v, phero_map, visibility_m, alpha, beta):
+        # array of probabilities
+        p = np.zeros(len(visibility_m[0]))
+        # index of destination 
+        j_v_L = np.arange(len(visibility_m[0]))
+        for j_v in j_v_L:
+            den = 0
+            for j, n in enumerate(visibility_m[i_v]):
+                # for each destination
+                if j != i_v:
+                    den_to_add = m.pow(phero_map[i_v, j], alpha) * m.pow(n, beta)
+                    if j == j_v:
+                        num = den_to_add
+                    den += den_to_add
+                else : pass
+            p[j_v] = num / den
+        print(p)
+        return np.max(p)
+
+    def apply_choice(self):
+        pass
+
--- World.py
+++ World.py
@@ -1,35 +1,45 @@
 import numpy as np
+from Fourmis import Fourmi
 np.random.seed(42)
 class World:
     def __init__(self, nb_cities, nb_ants):
-        self.nb_cities = nb_cities
-        self.nb_ants = nb_ants
+        self.n = nb_cities
+        self._B = []
+        self._m = nb_ants
+        self._ants = None
         self.map = None
         self.c = 0.01
         self.initialize()
 
     def initialize(self):
         self.creat_map()
-        self.creat_phe_map()
+        self.creat_phero_map()
         self.creat_place_ants()
 
     def creat_map(self):
         # self.map = np.arange()
-        _map = np.random.randint(0, self.nb_cities*2, (self.nb_cities,self.nb_cities))
+        _map = np.random.randint(0, self.n*2, (self.n,self.n))
         _map -= np.diag(_map.diagonal())
         _map = _map + _map.T
         self.map = _map
+        print(self.map)
+        # np.linspace()
 
-    def creat_phe_map(self):
-        _phe_map = np.full((self.nb_cities,self.nb_cities), self.c)
-        _phe_map -= np.diag(_phe_map.diagonal())
-        print(_phe_map)
+    def creat_phero_map(self):
+        _phero_map = np.full((self.n,self.n), self.c)
+        _phero_map -= np.diag(_phero_map.diagonal())
+        print(_phero_map)
+
+    def creat_place_ants(self):
+        self._ants = np.empty(self._m, dtype=Fourmis)
+        for i in self._m:
+            ant = Fourmis(i, )
+        for city in self.map:
+            
 
     def run_one_cycle(self):
         pass
 
-    def creat_place_ants(self):
-        pass
     
 if __name__ == '__main__':
     nb_cities = 5
