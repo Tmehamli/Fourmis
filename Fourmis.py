90c95f64750896f0221e70cb48b3d0c282d0b46d
tmehamli <tmehamli@ensc.fr>
2020-01-29 21:00:03 +0100

    v2 complete

--- Fourmis.py
+++ Fourmis.py
@@ -3,33 +3,54 @@ import math as m
 class Fourmi:
     def __init__(self, iden, stat_pos):
         self.identity = iden
-        self.memory = []
         self.start_pos = stat_pos
-        self.t = 0
+        self.memory = None
+        self.current_pos = None
+        self.l_k = None
+        self.LK = None
+        # self.t = 0
         # self.initialize()
 
     def initialize(self):
-        pass
+        self.l_k = 0
+        self.LK = []
+        self.memory = []
+        self.current_pos = self.start_pos
+        self.memory.append(self.current_pos)
 
-    def make_choice(self, i_v, phero_map, visibility_m, alpha, beta):
+    def make_choice(self, i_v, phero_map, dist_m, alpha, beta):
         # array of probabilities
-        p = np.zeros(len(visibility_m[0]))
-        # index of destination 
-        j_v_L = np.arange(len(visibility_m[0]))
-        for j_v in j_v_L:
-            den = 0
-            for j, n in enumerate(visibility_m[i_v]):
-                # for each destination
-                if j != i_v:
-                    den_to_add = m.pow(phero_map[i_v, j], alpha) * m.pow(n, beta)
-                    if j == j_v:
-                        num = den_to_add
-                    den += den_to_add
-                else : pass
-            p[j_v] = num / den
-        print(p)
-        return np.max(p)
+        nb_dest = len(dist_m[0])
+        p = np.zeros(nb_dest)
+        if len(self.memory) < nb_dest:
+            # index of destination 
+            j_v_L = np.arange(nb_dest)
+            for j_v in j_v_L:
+                den = 0
+                num = 0
+                if j_v not in self.memory:
+                    for j, d in enumerate(dist_m[i_v]):
+                        # for each destination
+                        if j != i_v:
+                            den_to_add = m.pow(phero_map[i_v, j], alpha) * m.pow(1/d, beta)
+                            if j == j_v:
+                                num = den_to_add
+                            den += den_to_add
+                        else : pass
+                    p[j_v] = num / den
+        # ending the path with start pos
+        else:
+            p[self.start_pos] = 1
+        print(self.identity)
+        print(dist_m)
+        choice = np.argmax(p)
+        print([i_v, choice])
+        self.l_k += dist_m[i_v, choice]
+        self.LK.append(self.l_k)
+        return choice
 
-    def apply_choice(self):
+    def apply_choice(self, new_pos):
+        self.current_pos = new_pos
+        self.memory.append(new_pos)
         pass
 
--- World.py
+++ World.py
@@ -1,38 +1,44 @@
 import numpy as np
+import copy
 from Fourmis import Fourmi
 np.random.seed(42)
 
 class World:
-
-    def __init__(self, nb_cities, nb_ants):
+    c = .001
+    Q = 100
+    alpha = 1
+    beta = 0.25
+    rho = .5
+    def __init__(self, nb_epoch, nb_cities, nb_ants):
         self.n = nb_cities
-        self._B = []
         self._m = nb_ants
+        self.nb_epoch = nb_epoch
         self._ants = None
-        self.map = None
+        self.dist_map = None
         self.phero_map = None
-        self.c = 0.01
-        self.alpha = 0.5
-        self.beta = 0.5
-        self.initialize()
-
-    def initialize(self):
         self.creat_map()
         self.creat_phero_map()
         self.creat_place_ants()
+        self.best_lk = 10000
+        self.best_path = []
+        # self.initialize()
+
+    def initialize(self):
+        [ant.initialize() for ant in self._ants]
 
     def creat_map(self):
-        _map = np.random.randint(0, self.n*2, (self.n,self.n))
-        _map -= np.diag(_map.diagonal())
-        _map = _map + _map.T
-        self.map = _map
+        _map = np.random.randint(1, self.n, (self.n,self.n))
+        # _map -= np.diag(_map.diagonal())
+        _map = _map + _map.T 
+        self.dist_map = _map
         # print(self.map)
 
     def creat_phero_map(self):
         _phero_map = np.full((self.n,self.n), self.c)
         _phero_map -= np.diag(_phero_map.diagonal())
         self.phero_map = _phero_map
-        # print(_phero_map)
+        # self.phero_map = copy.deepcopy(1/(self.dist_map+np.eye(self.n)))
+        # print(self.phero_map)
 
     def creat_place_ants(self):
         self._ants = np.empty(self._m, dtype=object)
@@ -42,14 +48,50 @@ class World:
             start_pos = not_placed_on.pop(index)
             self._ants[i] = Fourmi(i, start_pos)
         # print([ant.start_position for ant in self._ants])
-            
+
+    def run_one_ite(self):
+        for ant in self._ants:
+            new_pos = ant.make_choice(ant.current_pos, self.phero_map, self.dist_map, self.alpha, self.beta)
+            ant.apply_choice(new_pos)
+        # print([ant.memory for ant in self._ants])
+
+    def update_phero_map(self):
+        new_phero_m = copy.deepcopy(self.phero_map)
+        for ant in self._ants:
+            for i in range(1, len(ant.memory)):
+                l, c = ant.memory[i-1], ant.memory[i]
+                if ant.LK[i-1] == 0:
+                    print(l,c)
+                new_phero_m[l,c] += self.Q / ant.LK[i-1]
+            if ant.l_k < self.best_lk:
+                self.best_lk = ant.l_k
+                self.best_path = ant.memory
+        for l, phero_l in enumerate(self.phero_map):
+            for c, phero in enumerate(phero_l):
+                new_phero_m[l,c] += phero * self.rho
+        self.phero_map = copy.deepcopy(new_phero_m)
 
     def run_one_cycle(self):
+        self.initialize()
+        for t in range(self.n):
+            self.run_one_ite()
+        self.update_phero_map()
         for ant in self._ants:
-            print(ant.make_choice(ant.start_pos, self.phero_map, self.map, self.alpha, self.beta))
-    
+            print(ant.l_k, ant.memory)
+        print("\n")
+
+    def run_simulation(self):
+        for i in range(self.nb_epoch):
+            self.run_one_cycle()
+            # https://www.csestack.org/python-check-if-all-elements-in-list-are-same/
+            if all(self._ants[0].memory == ant.memory for ant in self._ants):
+                break
+        return self.best_path
+
 if __name__ == '__main__':
-    nb_cities = 3
-    nb_ants = 1
-    world = World(nb_cities, nb_ants)
-    world.run_one_cycle()
+    nb_cities = 7
+    nb_ants = 2
+    nb_epoch = 3
+    world = World(nb_epoch, nb_cities, nb_ants)
+    print(world.run_simulation())
+
